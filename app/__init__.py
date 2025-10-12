1cccd446615ae5379139cf4877bf7385727ac32a
bsreevardhan <Sreevardhanbalaji1@email.com>
2025-10-13 01:56:05 +0530

    Replace old folder structure with new code

--- app/__init__.py
+++ app/__init__.py
@@ -2,24 +2,28 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_login import LoginManager
-from config import Config
 from flask_wtf.csrf import CSRFProtect
+from flask_migrate import Migrate   # ✅ Import Flask-Migrate
+from config import Config
 
-csrf = CSRFProtect()  # create instance
-
+csrf = CSRFProtect()
 db = SQLAlchemy()
 mail = Mail()
 login_manager = LoginManager()
+migrate = Migrate()   # ✅ Create Migrate instance
 
 def create_app():
-    app = Flask(__name__,template_folder='templates')
+    app = Flask(__name__, template_folder='templates')
     app.config.from_object(Config)
 
+    # Initialize extensions
     db.init_app(app)
     mail.init_app(app)
     login_manager.init_app(app)
-    csrf.init_app(app)  # 🔹 Important!
+    csrf.init_app(app)
+    migrate.init_app(app, db)  # ✅ Initialize Flask-Migrate with app & db
 
+    # Register blueprints
     from app.routes.public_routes import public_bp
     from app.routes.admin_routes import admin_bp
     from app.routes.auth_routes import auth_bp
